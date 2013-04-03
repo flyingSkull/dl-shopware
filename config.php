cb6d91c613a3610fea68f1af24064e034ff4eea6
Ricky Flugrat <ricky.flugrat@googlemail.com>
2013-04-03 14:33:09 +0200

    init shopware

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'db' => 
+  array (
+    'username' => 'root',
+    'password' => 'root',
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'usr_p159946_3',
+  ),
+);
