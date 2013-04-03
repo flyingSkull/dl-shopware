e8d5d56d95956a37c0fba3258e501375f23ad58f
Ricky Flugrat <ricky.flugrat@googlemail.com>
2013-04-03 16:23:04 +0200

    config.php

--- config.php
+++ config.php
@@ -1,10 +1,18 @@
 <?php return array (
   'db' => 
   array (
+
+      'username' => 'p159946',
+    'password' => 'H7jEnVUP',
+    'dbname' => 'usr_p159946_3',
+    'host' => 'db1107.mydbserver.com',
+    'port' => '3306'
+     /*
     'username' => 'root',
     'password' => 'root',
     'host' => 'localhost',
     'port' => '3306',
     'dbname' => 'usr_p159946_3',
+     */
   ),
 );
