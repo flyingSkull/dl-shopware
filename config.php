01b1d469eded7de6aa14e8ed73004e1477a797fc
p159946 <p159946@localdomain>
2013-07-24 15:19:09 +0200

    config remote

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php return array (
   'db' => 
   array (
-/*
-      'username' => 'p159946',
+
+    'username' => 'p159946',
     'password' => 'H7jEnVUP',
     'dbname' => 'usr_p159946_3',
     'host' => 'db1107.mydbserver.com',
-    'port' => '3306'
-*/
+    'port' => '3306',
 
+/*
     'username' => 'root',
     'password' => 'root',
     'host' => 'localhost',
     'port' => '3306',
     'dbname' => 'usr_p159946_3',
-
+*/
   ),
 );
